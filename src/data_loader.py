5c60ad0e69d69ee2a597e62e6ea3101dd624e46d
Hari Krishna Kumar <harikrishnakumar368@gmail.com>
2026-06-15 21:32:37 +0530

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,9 +2,12 @@ import pandas as pd
 import os
 
 def load_data():
-    #os.chdir(r'https://raw.github.com/HariKrishnaKumar-watersprinter/Tesla-Stock-Price-Prediction---using-deeplearning-model-AI-ML-intern-labmentix/main')
-    path = os.path.join(os.getcwd(), "data", "TSLA.csv")
+    # Direct raw GitHub URL (most reliable for Streamlit)
+    url = "https://raw.githubusercontent.com/HariKrishnaKumar-watersprinter/Tesla-Stock-Price-Prediction---using-deeplearning-model-AI-ML-intern-labmentix/main/data/TSLA.csv"
     
-    df = pd.read_csv(path)
-    
-    return df
+    try:
+        df = pd.read_csv(url)
+        return df
+    except Exception as e:
+        st.error(f"Failed to load data: {e}")
+        return None
